40ed25018f216e0ccd6ec98ed8dc13f3140b8f01
James Woods <james.d.t.woods@gmail.com>
2023-10-17 11:53:46 -0500

    Finished with the first feature

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,30 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+
+function addToCollection(collection, title, artist, yearPublished) {
+  console.log('in addToCollectioin:', collection, title, artist, yearPublished);
+  collection.push({title, artist, yearPublished});
+  return {title, artist, yearPublished};
+} //end addToCollection
+
+console.log('running addToCollection: ', addToCollection(myCollection, 'Definately Maybe', 'Oasis', 1994));
+console.log('running addToCollection: ', addToCollection(myCollection, 'Dookie', 'Green Day', 1994));
+console.log('running addToCollection: ', addToCollection(myCollection, 'Odelay', 'Beck', 1996));
+console.log('running addToCollection: ', addToCollection(myCollection, 'White Blood Cells', 'The White Stripes', 2001));
+console.log('running addToCollection: ', addToCollection(myCollection, 'Is This It', 'The Strokes', 2001));
+console.log('running addToCollection: ', addToCollection(myCollection, 'Icky Thump', 'The White Stripes', 2007));
+
+
+console.log(myCollection);
+
+
+
+
+
+
+
 
 
 
